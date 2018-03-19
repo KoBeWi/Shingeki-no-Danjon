9d5f0f7d3c93a78f258183aabb0212d7a851fd07
KoBeWi <kobewi4e@gmail.com>
2018-03-19 10:39:25 +0100

    Trochu rollback

---
+++ Resources/Tilesets/FloorTileGenerator.rb
@@ -0,0 +1,7 @@
+TILE = '[node name="Floor2" type="Sprite" parent="." index="26"]
+
+position = Vector2( 80, 400 )
+texture = ExtResource( 1 )
+region_enabled = true
+region_rect = Rect2( 80, 320, 80, 80 )
+_sections_unfolded = [ "Region" ]'
