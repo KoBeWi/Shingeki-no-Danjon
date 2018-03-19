bed97064ead7af34b153b5f7be01e94b6908125c
KoBeWi <kobewi4e@gmail.com>
2018-03-19 10:51:33 +0100

    Generator tilesetów (tak trochę)

--- Resources/Tilesets/FloorTileGenerator.rb
+++ Resources/Tilesets/FloorTileGenerator.rb
@@ -1,7 +1,11 @@
-TILE = '[node name="Floor2" type="Sprite" parent="." index="26"]
+TILE = '[node name="Floor2" type="Sprite" parent="." index="%{id}"]
 
-position = Vector2( 80, 400 )
+position = Vector2( %{x}, 400 )
 texture = ExtResource( 1 )
 region_enabled = true
-region_rect = Rect2( 80, 320, 80, 80 )
+region_rect = Rect2( %{rectx}, %{recty}, 80, 80 )
 _sections_unfolded = [ "Region" ]'
+
+10.times do |i|
+	puts(TILE % {id: 25+i, rectx: i*80, recty: 320, x: i*80})
+end
