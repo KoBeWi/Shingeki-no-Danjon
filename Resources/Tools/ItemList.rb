826c6026ebd980c28fcbe65ad791cb0922df9308
Tomasz Chabora <kobewi4e@gmail.com>
2018-06-10 21:50:47 +0200

    kategorie (już na serio)

--- Resources/Tools/ItemList.rb
+++ Resources/Tools/ItemList.rb
@@ -4,7 +4,7 @@ items = []
 
 (Dir.entries("../Items") - [".", ".."]).sort{|entry1, entry2| entry1.rjust(8, "0") <=> entry2.rjust(8, "0")}.each do |file|
 	item = JSON.parse(File.readlines("../Items/#{file}").join("\n"))
-	puts "#{file.chomp(".json").rjust(3, " ")} -> #{item["name"]}"
+	puts "#{file.chomp(".json").rjust(3, " ")} -> #{item["name"]} (#{item["type"]})"
 end
 
 gets
